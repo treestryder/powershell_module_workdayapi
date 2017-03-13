09545bc4031710a07b6cfa2270f6642e2cb8c7b8
Nathan Hartley <nhartley@peckham.org>
2017-03-13 11:33:45 -0400

    2015-12-22T15:21:41-05:00 Added Get-WorkdayWorkerEmail, Update-WorkdayWorkerEmail and the Worker WID to the output of Get-WorkdayWorkerPhone.

--- scripts/Get-WorkdayWorkerEmail.ps1
+++ scripts/Get-WorkdayWorkerEmail.ps1
@@ -1,10 +1,10 @@
-﻿function Get-WorkdayWorkerPhone {
+﻿function Get-WorkdayWorkerEmail {
 <#
 .SYNOPSIS
-    Returns a Worker's phone numbers.
+    Returns a Worker's email addresses.
 
 .DESCRIPTION
-    Returns a Worker's phone numbers as custom Powershell objects.
+    Returns a Worker's email addresses as custom Powershell objects.
 
 .PARAMETER EmployeeId
     The Worker's Employee Id at Workday. This cmdlet does not currently
@@ -24,7 +24,7 @@
 
 .EXAMPLE
     
-Get-WorkdayWorkerPhone -EmpoyeeId 123
+Get-WorkdayWorkerEmail -EmpoyeeId 123
 
 Type          Number            Primary Public
 ----          ------            ------- ------
@@ -56,18 +56,18 @@ Work/Landline +1 (987) 654-3210 1         True
     $numberTemplate = [pscustomobject][ordered]@{
         WorkerWid        = $w.Get_Workers_Response.Response_Data.Worker.Worker_Reference.ID | where {$_.type -eq 'WID'} | select -ExpandProperty '#text'
         WorkerDescriptor = $w.Get_Workers_Response.Request_References.Worker_Reference.Descriptor
-        Type    = $null
-        Number  = $null
-        Primary = $null
-        Public  = $null
+        Type             = $null
+        Email            = $null
+        Primary          = $null
+        Public           = $null
     }
 
-    $w.Get_Workers_Response.Response_Data.FirstChild.Worker_Data.Personal_Data.Contact_Data.Phone_Data | foreach {
+    $w.Get_Workers_Response.Response_Data.FirstChild.Worker_Data.Personal_Data.Contact_Data.Email_Address_Data | foreach {
         $o = $numberTemplate.PsObject.Copy()
-        $o.Type = $_.Usage_Data.Type_Data.Type_Reference.Descriptor + '/' + $_.Phone_Device_Type_Reference.Descriptor
-        $o.Number = $_.Formatted_Phone
-        $o.Primary = [bool]$_.Usage_Data.Type_Data.Primary
-        $o.Public = [bool]$_.Usage_Data.Public
+        $o.Type = $_.Usage_Data.Type_Data.Type_Reference.Descriptor
+        $o.Email = $_.Email_Address
+        $o.Primary = $_.Usage_Data.Type_Data.Primary -match '1|true'
+        $o.Public = $_.Usage_Data.Public -match '1|true'
         Write-Output $o
     }
 }
---
+++ scripts/Update-WorkdayWorkerEmail.ps1
@@ -0,0 +1,63 @@
+﻿function Update-WorkdayWorkerEmail {
+<#
+.SYNOPSIS
+    Updates a Worker's email in Workday, only if it is different.
+
+.DESCRIPTION
+    Updates a Worker's email in Workday, only if it is different.
+    Change requests are always recorded in Workday's audit log even when
+    the email is the same. Unlike Set-WorkdayWorkerEmail, this cmdlet
+    first checks the current email before requesting a change. 
+
+.PARAMETER EmployeeId
+    The Worker's Employee Id at Workday. This cmdlet does not currently
+    support Contengent Workers or referencing workers by WID.
+
+.PARAMETER WorkEmail
+    Sets the Workday primary, public, Work email for a Worker. This cmdlet does not
+    currently support other email types. Also excepts the alias EmailAddress.
+
+.PARAMETER Human_ResourcesUri
+    Human_Resources Endpoint Uri for the request. If not provided, the value
+    stored with Set-WorkdayEndpoint -Endpoint Human_Resources is used.
+
+.PARAMETER Username
+    Username used to authenticate with Workday. If empty, the value stored
+    using Set-WorkdayCredential will be used.
+
+.PARAMETER Password
+    Password used to authenticate with Workday. If empty, the value stored
+    using Set-WorkdayCredential will be used.
+
+.EXAMPLE
+    
+Update-WorkdayWorkerEmail -EmpoyeeId 123 -WorkEmail test@example.com
+
+#>
+
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string]$EmployeeId,
+		[Parameter(Mandatory = $true)]
+        [Alias('EmailAddress')]
+		[string]$WorkEmail,
+		[string]$Human_ResourcesUri,
+		[string]$Username,
+		[string]$Password,
+        [switch]$Passthru
+	)
+
+    if ([string]::IsNullOrWhiteSpace($Human_ResourcesUri)) { $Human_ResourcesUri = $WorkdayConfiguration.Endpoints['Human_Resources'] }
+    
+    $current = Get-WorkdayWorkerEmail -EmployeeId $EmployeeId -Human_ResourcesUri:$Human_ResourcesUri -Username:$Username -Password:$Password
+
+    $currentWorkEmail = $current | where { $_.Type -eq 'Work' -and $_.Primary } | Select -First 1 -ExpandProperty Email
+
+    Write-Verbose "Current: $currentWorkEmail Proposed: $WorkEmail"
+    if ($currentWorkEmail -ne $WorkEmail) {
+        Set-WorkdayWorkerEmail -EmployeeId $EmployeeId -WorkEmail $WorkEmail -Human_ResourcesUri:$Human_ResourcesUri -Username:$Username -Password:$Password | where {$Passthru} | Write-Output
+        Write-Verbose "     Email updated."
+    }
+}
