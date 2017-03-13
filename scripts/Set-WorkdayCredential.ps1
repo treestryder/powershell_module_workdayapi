356a4eac518ebc2b9433a93efef29533ac86f5d2
Nathan Hartley <nhartley@peckham.org>
2017-03-13 11:33:23 -0400

    2015-12-03T15:08:22-05:00 Rebuilt Set-WorkdayCredential

--- scripts/Set-WorkdayCredential.ps1
+++ scripts/Set-WorkdayCredential.ps1
@@ -1,1 +1,33 @@
-function Set-WorkdayCredential {}
+<#
+.SYNOPSIS
+    Sets the default Workday API credentials.
+
+.DESCRIPTION
+    Sets the default Workday API credentials. Configuration values can
+    be securely saved to a user's profile using Save-WorkdayConfiguration.
+
+.PARAMETER Credential
+    A standard Powershell Credential object. Optional.
+    
+.EXAMPLE
+    Set-WorkdayCredential
+
+    This will prompt the user for credentials and save them in memory.
+
+.EXAMPLE
+    $cred = Get-Credential -Message 'Custom message...' -UserName 'Custom Username'
+    Set-WorkdayCredential -Credential $cred
+
+    This demonstrates prompting the user with a custom message and default username.
+
+#>
+
+function Set-WorkdayCredential {
+    [CmdletBinding()]
+    param (
+        [PSCredential]$Credential = $(Get-Credential -Message 'Enter Workday API credentials.')
+    )
+
+    $WorkdayConfiguration.Credential = $Credential
+}
+
