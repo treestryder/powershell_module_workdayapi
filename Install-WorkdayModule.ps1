0e4b08cf199e55fcf7ebe7e1ea9bbfbd7db43ebf
Nathan Hartley <nhartley@peckham.org>
2017-03-13 11:34:26 -0400

    2016-01-06T09:58:40-05:00 Added a simple installation script that puts the module files in place.

---
+++ Install-WorkdayModule.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding()]
+param (
+    [string]$InstallPath = (Join-Path $env:ProgramFiles 'WindowsPowerShell\Modules\WorkdayApi'),
+    [switch]$Force
+)
+
+$sourceFiles = @(
+    '.\en-US\',
+    '.\samples\',
+    '.\scripts\',
+    '.\tests\',
+    '.\WorkdayApi.ps*'
+)
+
+
+if (Test-Path $InstallPath) {
+    if ($Force) {
+        Remove-Item -Path $InstallPath\* -Recurse
+    } else {
+        Write-Warning "Module already installed at `"$InstallPath`" use -Force to overwrite installation."
+        return
+    }
+} else {
+    New-Item -Path $InstallPath -ItemType Directory | Out-Null
+}
+
+Push-Location $PSScriptRoot
+
+Copy-Item -Path $sourceFiles -Destination $InstallPath -Recurse
+
+Pop-Location
+
+Import-Module -Name WorkdayApi -Verbose
